479b53d22cb424f3a2ba5e71d9baa9fe8d25df2c
546736om <546736om@OPC1509.nisshin-oillio.local>
2016-04-01 11:43:04 +0900

    Change-Id: I368bd2fc1899be51cf4cc91a4c181ecbce198bf7

---
+++ src/com/walk_nie/tbtool/Main.java
@@ -0,0 +1,14 @@
+package com.walk_nie.tbtool;
+
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
+
+}
---
+++ src/com/walk_nie/tbtool/asisc/AsiscShoesParser.java
@@ -0,0 +1,6 @@
+package com.walk_nie.tbtool.asisc;
+
+
+public class AsiscShoesParser {
+
+}
