ba81a06166dd11dab72a5c0a6a2efeeac16b5e65
niehp <niehp@192.168.1.8>
2016-12-25 02:07:33 +0900

    uu

---
+++ src/com/walk_nie/taobao/montBell/StockObject.java
@@ -0,0 +1,11 @@
+package com.walk_nie.taobao.montBell;
+
+
+public class StockObject {
+
+	public String productId;
+	public String colorName;
+	public String sizeName;
+	public boolean isStock;
+	 
+}
