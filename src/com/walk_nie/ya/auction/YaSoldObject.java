e734df07231ae940c4b8bb8ce4f36f158aa36cfd
niehp <niehp@192.168.1.7>
2018-07-10 23:14:30 +0900

    aaa

---
+++ src/com/walk_nie/ya/auction/YaSoldObject.java
@@ -0,0 +1,11 @@
+package com.walk_nie.ya.auction;
+
+
+public class YaSoldObject {
+	String auctionId="";
+	String title="";
+	String price="";
+	String latestTime="";
+	String obider="";
+	String statusMsg="";
+}
