98792104ecd429cb04f5d9263d03e2279163e145
niehp <niehp@192.168.1.5>
2018-10-26 00:53:05 +0900

    i

---
+++ src/com/walk_nie/object/CrObject.java
@@ -0,0 +1,15 @@
+package com.walk_nie.object;
+
+public class CrObject {
+    public String id;
+    public String crBrand;//jcb,master,unitpay
+    public String numb1;
+    public String numb2;
+    public String numb3;
+    public String numb4;
+    public String expiredMon;
+    public String expiredYear;
+    public String scode;
+    public String meigi1;
+    public String meigi2;
+}
